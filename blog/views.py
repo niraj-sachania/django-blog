0628ceb4c824cca6040aa84331cd0ee6504f9f8d
niraj-sachania <>
2026-01-21 14:29:21 +0000

    feat: add comments and count display to posts

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,13 @@ from .models import Post
 
 # Create your views here.
 
+
 class PostList(generic.ListView):
     # model = Post  # Class-based generic views
     queryset = Post.objects.filter(status=1).order_by("created_on")
     template_name = "blog/index.html"
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
@@ -26,9 +28,16 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post, "coder": "Niraj Sachania"},
+        {
+            "post": post,
+            "coder": "Niraj Sachania",
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
